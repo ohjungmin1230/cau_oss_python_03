2fe12bac6f1631aff92893875b002066f4fca355
ohjungmin1230 <127947442+ohjungmin1230@users.noreply.github.com>
2023-03-23 21:55:05 +0900

    Update volume_calc.py
    변수를 의미있게 바꿈
    총 길이에 따른 요금 계산 기능 추

--- volume_calc.py
+++ volume_calc.py
@@ -1,5 +1,20 @@
-x=input("가로:")
-y=input("세로:")
-h=input("높이:")
-v=float(x)*float(y)*float(h)
-print("박스의 부피는"+str(v)+"입니다.")
+length=input("가로:")
+width=input("세로:")
+height=input("높이:")
+volume=float(length)*float(width)*float(height)
+print("부피는"+str(volume)+"입니다.")
+
+total_length=float(length)+float(width)+float(height)
+
+if total_length <= 80:
+    charge = 5000
+elif total_length <= 100:
+    charge = 10000
+elif total_length <= 120:
+    charge = 13000
+else:
+    charge = "unavailable"
+
+print("total length:", total_length)
+print("charge:", charge)
+
